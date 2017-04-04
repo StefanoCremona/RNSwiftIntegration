98ccd981af2f3a9f4f3dbfa024b67f78fb2fb600
Stefano MacMini <stefano.cremona@gmail.com>
2017-04-04 18:33:16 +0100

    Add empty swift project

---
+++ RNSwiftIntegration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RNSwiftIntegration
+//
+//  Created by Stefano Cremona on 04/04/2017.
+//  Copyright © 2017 Stefano Cremona. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
