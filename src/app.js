e3999921824da6d3cb6c6975d56d6e6c8d881887
Stefano Cremona <stefano.cremona@gmail.com>
2017-05-14 13:53:16 +0100

    Add favorite team field in RN view

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { NativeModules, TouchableOpacity } from 'react-native';
+import { NativeModules, TouchableOpacity, TextInput } from 'react-native';
 
 const MyNativeBridge = NativeModules.RNBridge;  //Use the swift name file
 
 class App extends Component {
+
+    state = {
+        favouriteTeamName: null
+    }
 
     render() {
         return (
@@ -15,17 +19,38 @@ class App extends Component {
                     justifyContent: 'center'
                 }}
             >
-            <Text>Hello {this.props.myBestFriendName}!</Text>
-            <TouchableOpacity
-                onPress={() => MyNativeBridge.dismissRNViewController()}
-            >
-                <Text style={{ color: 'red' }}>Go Back to Native</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                onPress={() => MyNativeBridge.callToCheckOutSegue()}
-            >
-                <Text style={{ color: 'blue' }}>Go To the Checkout</Text>
-            </TouchableOpacity>
+                <Text>{this.props.myBestFriendName}'s favourite football team:</Text>
+                <TextInput
+                    style={{ 
+                        height: 40, 
+                        borderColor: 'gray', 
+                        borderWidth: 1,
+                        margin: 20,
+                        padding: 10
+                    }}
+                    onChangeText={(text) => this.setState({text})}
+                    placeholder={'eg: AS ROMA'}
+                    value={this.state.favouriteTeamName}
+                />
+                <View
+                    style={{ 
+                        flexDirection: 'row',
+                        margin: 10
+                    }}
+                >
+                    <TouchableOpacity
+                        style={{ flex: 1, alignItems: 'center' }}
+                        onPress={() => MyNativeBridge.dismissRNViewController()}
+                    >
+                        <Text style={{ color: 'red' }}>Go Back to Native</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={{ flex: 1, alignItems: 'center' }}
+                        onPress={() => MyNativeBridge.callToCheckOutSegue()}
+                    >
+                        <Text style={{ color: 'blue' }}>Go To the Checkout</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         );
     }
